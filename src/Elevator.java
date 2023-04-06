c10f9fbedaf6abdceac6c8067caf040acb32ad10
Kevin <kevindoyleie@gmail.com>
2023-04-06 08:49:28 +0100

    exercise

---
+++ src/Elevator.java
@@ -0,0 +1,59 @@
+import java.util.LinkedList;
+
+public class Elevator
+{
+    private int floor;
+    private final LinkedList<Action> actions = new LinkedList<Action>();
+
+    /* for unit tests */
+    public int getFloor() {
+        return this.floor;
+    }
+    /* for unit tests */
+    public void setFloor(int floor)
+    {
+        this.floor = floor;
+    }
+
+    protected enum Action {
+        UP_ONE,
+        DOWN_ONE,
+        OPEN_CLOSE,
+        NO_OP
+    }
+
+    public Elevator() {
+        this.floor = 1;
+    }
+
+    public void requestFloor(int floorNumber) {
+        actions.add(Action.OPEN_CLOSE);
+        while (this.floor != floorNumber) {
+            int i = 0;
+            if (floorNumber > this.floor) {
+                for (i = this.floor; i <= floorNumber; i++) {
+                    System.out.println("Going up..." + i);
+                    this.actions.add(Action.UP_ONE);
+                }
+                i--;
+            } else {
+                for (i = this.floor; i >= floorNumber; i--) {
+                    System.out.println("Going down..." + i);
+                    this.actions.add(Action.DOWN_ONE);
+                }
+                i++;
+            }
+            this.floor = i;
+            if (floorNumber == this.floor)
+                this.actions.add(Action.OPEN_CLOSE);
+
+            System.out.println("Hello, opening the door, we are on " + i);
+            this.actions.add(Action.NO_OP);
+        }
+    }
+
+    public Action getAction() {
+        return actions.removeFirst();
+    }
+}
+
---
+++ src/ElevatorTest.java
@@ -0,0 +1,66 @@
+import org.junit.Test;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ElevatorTest
+{
+    private Elevator elevator = null;
+
+    @Before
+    public void setUp() {
+        elevator = new Elevator();
+    }
+
+    @Test
+    public void createElevator() {
+        assertNotNull(elevator);
+        assertEquals(1, elevator.getFloor());
+    }
+
+    @Test
+    public void whenGoFrom1to2() {
+        elevator.requestFloor(2);
+
+        assertEquals(Elevator.Action.OPEN_CLOSE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.OPEN_CLOSE, elevator.getAction());
+        assertEquals(Elevator.Action.NO_OP, elevator.getAction());
+        assertEquals(2, elevator.getFloor());
+    }
+
+    @Test
+    public void whenGoFrom2to1() {
+        elevator.setFloor(2);
+        elevator.requestFloor(1);
+
+        assertEquals(Elevator.Action.OPEN_CLOSE, elevator.getAction());
+        assertEquals(Elevator.Action.DOWN_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.DOWN_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.OPEN_CLOSE, elevator.getAction());
+        assertEquals(Elevator.Action.NO_OP, elevator.getAction());
+        assertEquals(1, elevator.getFloor());
+    }
+
+    @Test
+    public void whenGoFrom1to10() {
+        elevator.requestFloor(10);
+
+        assertEquals(Elevator.Action.OPEN_CLOSE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.UP_ONE, elevator.getAction());
+        assertEquals(Elevator.Action.OPEN_CLOSE, elevator.getAction());
+        assertEquals(Elevator.Action.NO_OP, elevator.getAction());
+        assertEquals(10, elevator.getFloor());
+    }
+}
